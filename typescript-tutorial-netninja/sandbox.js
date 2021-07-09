20ca3230bb36d5227788b77f60173999444052f6
puifaiba <puifai.ba@gmail.com>
2021-07-09 23:02:46 +0700

    compiled ts file to js

---
+++ typescript-tutorial-netninja/sandbox.js
@@ -0,0 +1,4 @@
+var user = "Choppo";
+console.log(user);
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
---
+++ typescript-tutorial-netninja/sandbox.ts
@@ -0,0 +1,7 @@
+const user = "Choppo"
+
+console.log(user)
+
+const inputs = document.querySelectorAll("input")
+
+console.log(inputs)
