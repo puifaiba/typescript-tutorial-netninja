fdb35699a1a19bebb97b04b25552d69f1f2df75f
puifaiba <puifai.ba@gmail.com>
2021-07-09 23:10:24 +0700

    used forEach on nodelist and compiled in watch mode

--- typescript-tutorial-netninja/sandbox.js
+++ typescript-tutorial-netninja/sandbox.js
@@ -2,3 +2,6 @@ var user = "Choppo";
 console.log(user);
 var inputs = document.querySelectorAll("input");
 console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
--- typescript-tutorial-netninja/sandbox.ts
+++ typescript-tutorial-netninja/sandbox.ts
@@ -5,3 +5,7 @@ console.log(user)
 const inputs = document.querySelectorAll("input")
 
 console.log(inputs)
+
+inputs.forEach((input) => {
+  console.log(input)
+})
