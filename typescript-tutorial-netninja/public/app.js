c1d9f5856515d4fac3f74f482bb72360b7697b8f
puifaiba <puifai.ba@gmail.com>
2021-07-15 23:31:38 +0530

    DOM selection and type casting

--- typescript-tutorial-netninja/public/app.js
+++ typescript-tutorial-netninja/public/app.js
@@ -1,5 +1,14 @@
 "use strict";
 var anchor = document.querySelector("a");
-if (anchor) {
-    console.log(anchor.href);
-}
+// anchor.href gives an error saying anchor might be null
+// using a bang at the end tells ts that it does exist
+// if (anchor) {
+//   console.log(anchor.href)
+// }
+console.log(anchor.href);
+// ts has special types for every DOM Element - const anchor is an HTMLAnchorElement
+// const form = document.querySelector("form")!
+// const form is an HTMLFormElement
+var form = document.querySelector(".new-item-form");
+// when selecting a class, ts doesn't know what type of Element it is so we need to specify
+console.log(form.children);
