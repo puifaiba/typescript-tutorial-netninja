2ba668b26e1f2fd118cf1a1e08081fc8509d49e6
puifaiba <puifai.ba@gmail.com>
2021-07-18 16:10:52 +0700

    using interface with clases

--- typescript-tutorial-netninja/src/models/invoice.ts
+++ typescript-tutorial-netninja/src/models/invoice.ts
@@ -1,7 +1,9 @@
 // ----------------------------------------------------- //
 // CLASSES
 
-export class Invoice {
+import {HasFormatter} from "../interfaces/HasFormatter"
+
+export class Invoice implements HasFormatter {
   constructor(
     readonly client: string,
     private details: string,
